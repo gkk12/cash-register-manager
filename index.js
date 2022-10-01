23bb052185b0ab00db55b1f29cc67d860da07398
Gautham Kamath K <gauthamkamathk24@gmail.com>
2022-10-01 10:08:10 +0200

    handled invalid inputs

--- index.js
+++ index.js
@@ -10,30 +10,46 @@ cashGiven.style.display = "None";
 cashGivenLabel.style.display = "None";
 
 //bonus question
-billAmount.addEventListener("keyup", function displaySecond() { 
+billAmount.addEventListener("keyup", function displaySecond() {
     if (billAmount.value > 0) {
         cashGiven.style.display = "inline-block";
         cashGivenLabel.style.display = "block";
     } else {
         cashGiven.style.display = "None";
         cashGivenLabel.style.display = "None";
+        showMessage("Invalid input value")
     }
 
 })
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        } else {
+    const cash = Number(cashGiven.value);
+    const bill = Number(billAmount.value);
+    if (cash > 0) {
+        console.log(bill)
+        console.log(cash)
+        if (cash == bill) {
+            showMessage("No change to return")
+            resetTable();
+        } else if (cash < bill) {
             showMessage("Cash provided should be greater than the bill amount")
+            resetTable();
+        } else {
+            const amountToBeReturned = cash - bill;
+            calculateChange(amountToBeReturned);
         }
     } else {
-        showMessage("Invalid Bill Amount")
+        showMessage("Invalid input values")
+        resetTable();
     }
 })
+
+function resetTable() {
+    for (let i = 0; i < availableNotes.length; i++) {
+        noOfNotes[i].innerText = "";
+    }
+}
 
 function calculateChange(amountToBeReturned) {
     for (let i = 0; i < availableNotes.length; i++) {
